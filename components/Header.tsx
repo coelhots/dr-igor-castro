72d631fa92f73263525fd4eb35b408784b16fd6d
Daniel Coelho <danielcoelho@ciandt.com>
2022-07-05 01:06:36 -0300

    alterações header e navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,42 @@
-import { Box, Container, Typography } from "@mui/material"
-import React from "react"
-import { styled } from "@mui/material/styles"
+import { CardMedia, Container, Typography, Box } from "@mui/material";
+import React from "react";
+import { styled } from "@mui/material/styles";
 
-const Wrapper = styled(Container)(
-  {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    maxHeight: "400px"
-  }
-)
+const LogoJanela = styled(Typography)({
+  position: "absolute",
+});
 
 const Header = () =>{
-  return(
-      <Wrapper sx={{
-        mt: "56px"
+  return (
+    <Box>
+      <Box sx={{position: "relative"}}>
+        <CardMedia sx={{filter: "brightness(35%)"}}
+              component="video"
+              autoPlay
+              loop
+              muted
+              src="/pexels-ron-lach-7584602.mp4"
+              />
+        <Box sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: "100%",
+        color: 'white',
       }}>
-        <Typography sx={{flexGrow: 2}}>Dr. Janela Bolado</Typography>
-
-        <Box component="img"
-        alt="Janela Bolado"
-        src="/janela.jpg"
-        sx={{
-          flexGrow: 3,
-          height: "100%",
-        }}
-        >
+          <Box sx={{ display: "flex", alignItems: "center",justifyContent: "center"}}>
+            <img alt="Logo I de Igor C de Castro" src="/logo-sem-nome.png" width="120px" height="120px"/>
+            <Typography sx={{fontFamily: "'Quicksand', sans-serif"}}>Dr. Igor Castro</Typography>
+          </Box>
         </Box>
-      </Wrapper>
-)
-}
+      </Box>
+    </Box>
+    
+  );
+};
+
 export default Header
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, IconButton, Container, Box } from "@mui/material";
+import { AppBar, IconButton, Container, Box, CardMedia, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -7,7 +7,7 @@ import Link from "@mui/material/Link";
 
 const CustomNavBar = styled(AppBar)({
   height: "56px",
-  backgroundColor: "#333333",
+  backgroundColor: "#214087",
 });
 
 const Wrapper = styled(Container)({
@@ -20,22 +20,27 @@ const Wrapper = styled(Container)({
 const LinkConfig = styled(Link)({
   display: "flex",
   justifyContent: "center",
-  color: "white",
-  fontFamily: "'Work Sans', sansSerif",
+  color: "#CFCFCF",
+  fontFamily: "'Quicksand', sans-serif",
   fontSize: "1.125rem",
 });
 
 const Navbar = () => {
   return (
     <>
-      <CustomNavBar position="sticky">
+      <CustomNavBar position="fixed">
         <Wrapper>
+          <IconButton>
+            <Box sx={{ display: "flex", alignItems: "center",justifyContent: "center" }}>
+            <img alt="Logo I de Igor C de Castro" src="/logo-sem-nome.png" width="50px" height="50px"/>
+              <Typography sx={{fontFamily: "'Quicksand', sans-serif", color: "#CFCFCF"}}>Dr. Igor Castro</Typography>
+            </Box>
 
-          <IconButton></IconButton>
+          </IconButton>
 
           <IconButton>
             <LinkConfig rel="noopener" underline="none" href="https://www.instagram.com/dr.igorcastro/" target="_blank">
-              <InstagramIcon sx={{ color: "white" }} />
+              <InstagramIcon sx={{ color: "CFCFCF" }} />
             </LinkConfig>
           </IconButton>
 
